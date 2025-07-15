dadefe3c41140cd6b7546b0f713b16b4a257a070
alphashaw <14175735+alphashaw@users.noreply.github.com>
2025-07-15 06:54:10 +0000

    monime-715: Update to version 1.0.0

--- Casks/monime-715.rb
+++ Casks/monime-715.rb
@@ -1,18 +1,17 @@
 cask "monime-715" do
-version "1.0.5"
-sha256 "5e3c8cbf8ddce05cd94cb87e52d70a724fb4c5fe0c836332a09bc0ae5e87a484"
+  version "1.0.0"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
 
-url "https://github.com/monimesl/monime-cli/releases/download/v1.0.4/Monime_715_macOS.zip"
-name "Monime 715.app"
-desc "Test and debug USSD flows (including payment code) in real time — no mobile device required."
-homepage "https://monime.io"
+  url "https://github.com/monimesl/monime-cli/releases/download/v1.0.0/Monime_715_macOS_macOS.zip"
+  name "Monime 715"
+  desc "Test and debug USSD flows (including payment code) in real time — no mobile device required."
+  homepage "https://monime.io"
 
-app "Monime 715.app"
+  app "Monime 715.app"
+
   postflight do
-    # `appdir` ensures it points to where Homebrew Cask installed the app
     executable_path = "#{appdir}/Monime 715.app/Contents/MacOS/monime-715"
- 
-    system "chmod", "0755", executable_path 
-    ohai "Set executable permissions for #{executable_path}"
+    system "chmod", "0755", executable_path
+    ohai "Setting executable permissions for #{executable_path}"
   end
 end
