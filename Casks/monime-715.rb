f8d510be1f367bb4294320b680cf374066f2246e
alphashaw <14175735+alphashaw@users.noreply.github.com>
2025-07-15 04:57:11 +0000

    monime-715: Update to version 1.0.3

--- Casks/monime-715.rb
+++ Casks/monime-715.rb
@@ -1,6 +1,6 @@
 cask "monime-715" do
 version "1.0.3"
-sha256 "e33dc768568b374b6b083e4941a3f3f59335ea1a14a8265ec5be695cfd73d536"
+sha256 "b7f8f7086b89f72418047c2105b71a4fc8e5188550f0560f13346e1e02de8618"
 
 url "https://github.com/monimesl/monime-cli/releases/download/v1.0.3/Monime_715_macOS.zip"
 name "Monime 715.app"
