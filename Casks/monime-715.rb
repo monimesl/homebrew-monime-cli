28c7952324469fa74f910939b6379933fc5c8729
alphashaw <14175735+alphashaw@users.noreply.github.com>
2025-07-15 03:58:07 +0000

    monime-715: Update to version 1.0.2

--- Casks/monime-715.rb
+++ Casks/monime-715.rb
@@ -1,8 +1,8 @@
 cask "monime-715" do
-version "1.0.1"
-sha256 "e8d250c7f6a7ff2c556950129dfa4112512edee1b2c0b54f42a93ed3334561de"
+version "1.0.2"
+sha256 "cdace0a8f2d0a3af40a1ff004e6f38518f335ee334d97662c38d8db0285282a4"
 
-url "https://github.com/monimesl/monime-cli/releases/download/v1.0.1/Monime_715_macOS.zip"
+url "https://github.com/monimesl/monime-cli/releases/download/v1.0.2/Monime_715_macOS.zip"
 name "Monime 715.app"
 desc "Test and debug USSD flows (including payment code) in real time — no mobile device required."
 homepage "https://monime.io"
