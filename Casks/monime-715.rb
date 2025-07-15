bff16e0d99f2dd65ad072cedb72c6051fd819628
Alpha Shaw <shawalpha5@gmail.com>
2025-07-15 05:40:21 +0000

    Update monime-715.rb

--- Casks/monime-715.rb
+++ Casks/monime-715.rb
@@ -1,5 +1,5 @@
 cask "monime-715" do
-version "1.0.4"
+version "1.0.5"
 sha256 "5e3c8cbf8ddce05cd94cb87e52d70a724fb4c5fe0c836332a09bc0ae5e87a484"
 
 url "https://github.com/monimesl/monime-cli/releases/download/v1.0.4/Monime_715_macOS.zip"
@@ -8,4 +8,11 @@ desc "Test and debug USSD flows (including payment code) in real time — no mob
 homepage "https://monime.io"
 
 app "Monime 715.app"
+  postflight do
+    # `appdir` ensures it points to where Homebrew Cask installed the app
+    executable_path = "#{appdir}/Monime 715.app/Contents/MacOS/monime-715"
+ 
+    system "chmod", "0755", executable_path 
+    ohai "Set executable permissions for #{executable_path}"
+  end
 end
