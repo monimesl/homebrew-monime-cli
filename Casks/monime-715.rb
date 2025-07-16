bd91227eb3ec80546e5b2bcc78f80ed3dbdd9f09
alphashaw <14175735+alphashaw@users.noreply.github.com>
2025-07-16 07:07:12 +0000

    monime-715: Bump to version 0.0.2

--- Casks/monime-715.rb
+++ Casks/monime-715.rb
@@ -1,8 +1,8 @@
 cask "monime-715" do
-  version "0.0.1"
-  sha256 "8f87d23ab9180c4eb63b9ea32d258e2102807c7c635630381a2ffe7daf338ea7"
+  version "0.0.2"
+  sha256 "ed5b5041edd05bd484a472ef7d85c8c9fd91618a9e178efa956656987c671c33"
 
-  url "https://github.com/monimesl/monime-cli/releases/download/v0.0.1/Monime_715_macOS.zip"
+  url "https://github.com/monimesl/monime-cli/releases/download/v0.0.2/Monime_715_macOS.zip"
   name "Monime 715"
   desc "Test and debug USSD flows (including payment code) in real time — no mobile device required."
   homepage "https://monime.io"
@@ -12,6 +12,5 @@ cask "monime-715" do
   postflight do
     executable_path = "#{appdir}/Monime 715.app/Contents/MacOS/monime-715"
     system "chmod", "0755", executable_path
-    ohai "Setting executable permissions for #{executable_path}"
   end
 end
